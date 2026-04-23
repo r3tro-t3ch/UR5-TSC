4da12e13467a08590996653915f60f545bd86838
Vishnu Joshi <joshi.vishnu1994@gmail.com>
2026-04-23 13:41:06 -0700

    Create test_ur_rtde_touch_and_sense.py

---
+++ test_ur_rtde_touch_and_sense.py
@@ -0,0 +1,15 @@
+# test from https://sdurobotics.gitlab.io/ur_rtde/api/api.html#_CPPv4N7ur_rtde20RTDEReceiveInterface24getActualCurrentAsTorqueEv
+
+
+# Recieve interface
+# getActualCurrentAsTorque()
+# getFtRawWrench()
+# getActualTCPForce()
+
+# control interface
+# getJointTorques()
+# startContactDetection()
+# readContactDetection()
+# stopContactDetection()
+# getJacobain()
+# getJacobianTimeDerivative
